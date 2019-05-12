a2b70c81a01a52c395ffbd6fd0ca938d13b7042e
Danial Khosravi <dani_khosravi@yahoo.com>
2019-05-12 18:14:36 +1000

    fp-course - State, monad and applicative

--- courses/fp-course/src/Course/FileIO.hs
+++ courses/fp-course/src/Course/FileIO.hs
@@ -85,24 +85,32 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile name content =
+  putStrLn ("============ " ++ name) >>
+  putStrLn content
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
+-- printFiles files = do
+--   let x = (uncurry printFile) <$> files
+--   let y = (void . sequence) x
+--   y
 printFiles =
-  error "todo: Course.FileIO#printFiles"
+  void . sequence . (<$>) (uncurry printFile)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
+-- getFile path = do
+--   content <- readFile path
+--   return (path, content)
 getFile =
-  error "todo: Course.FileIO#getFile"
+  lift2 (<$>) (,) readFile
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
@@ -110,21 +118,26 @@ getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
 getFiles =
-  error "todo: Course.FileIO#getFiles"
+  sequence . (<$>) getFile
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run path = do
+  content <- readFile path
+  results <- getFiles (lines content)
+  printFiles results
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
 main =
-  error "todo: Course.FileIO#main"
+  getArgs >>= \args ->
+    case args of
+      filename :. Nil -> run filename
+      _ -> putStrLn "usage: runhaskell io.hs filename"
 
 ----
 
--- courses/fp-course/src/Course/State.hs
+++ courses/fp-course/src/Course/State.hs
@@ -38,8 +38,8 @@ exec ::
   State s a
   -> s
   -> s
-exec =
-  error "todo: Course.State#exec"
+exec (State s) =
+  snd . s
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
@@ -48,8 +48,8 @@ eval ::
   State s a
   -> s
   -> a
-eval =
-  error "todo: Course.State#eval"
+eval (State s) =
+  fst . s
 
 -- | A `State` where the state also distributes into the produced value.
 --
@@ -58,7 +58,7 @@ eval =
 get ::
   State s s
 get =
-  error "todo: Course.State#get"
+  State (\s -> (s, s))
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
@@ -67,20 +67,21 @@ get =
 put ::
   s
   -> State s ()
-put =
-  error "todo: Course.State#put"
+put a =
+  State (\_ -> ((), a))
 
 -- | Implement the `Functor` instance for `State s`.
 --
 -- >>> runState ((+1) <$> State (\s -> (9, s * 2))) 3
 -- (10,6)
+
 instance Functor (State s) where
   (<$>) ::
     (a -> b)
     -> State s a
     -> State s b
-  (<$>) =
-    error "todo: Course.State#(<$>)"
+  f <$> State k =
+    State (\s -> let (a, t) = k s in (f a, t))
 
 -- | Implement the `Applicative` instance for `State s`.
 --
@@ -97,14 +98,18 @@ instance Applicative (State s) where
   pure ::
     a
     -> State s a
-  pure =
-    error "todo: Course.State pure#instance (State s)"
+  pure a = State (\s -> (a, s))
+
   (<*>) ::
     State s (a -> b)
     -> State s a
-    -> State s b 
-  (<*>) =
-    error "todo: Course.State (<*>)#instance (State s)"
+    -> State s b
+  State k <*> State l =
+    State (\s ->
+      let (f, t1) = k s
+          (a, t2) = l t1
+      in (f a, t2)
+    )
 
 -- | Implement the `Bind` instance for `State s`.
 --
@@ -118,8 +123,17 @@ instance Monad (State s) where
     (a -> State s b)
     -> State s a
     -> State s b
-  (=<<) =
-    error "todo: Course.State (=<<)#instance (State s)"
+  -- g =<< State l =
+  --   State (\s ->
+  --     let (a, t) = l s
+  --         State k = g a
+  --     in k t
+  --   )
+  g =<< State l =
+    State (\s ->
+      let (a, t) = l s
+      in runState (g a) t
+    )
 
 -- | Find the first element in a `List` that satisfies a given predicate.
 -- It is possible that no element is found, hence an `Optional` result.
@@ -140,8 +154,10 @@ findM ::
   (a -> f Bool)
   -> List a
   -> f (Optional a)
-findM =
-  error "todo: Course.State#findM"
+findM _ Nil =
+  pure Empty
+findM p (h :. t) =
+  (\q -> if q then pure (Full h) else findM p t) =<< p h
 
 -- | Find the first element in a `List` that repeats.
 -- It is possible that no element repeats, hence an `Optional` result.
